688dfcc0c4db7268dde9d0597fceaacf7e569c4c
skazska <skazska@gmail.com>
2019-09-26 16:00:41 +0300

    command stream on outStream

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const { Client } = require('ssh2');
 
 const connectionParams = require('./lib/connection-params');
 const Controller = require('./lib/controller');
-
 const argv = require('yargs-parser')(process.argv.slice(2));
+const { timedMessage } = require('./lib/utils');
 
 const conn = new Client();
-
 const params = connectionParams(argv);
+
+const logInfo = console.info;
+console.info = function(...args) {
+    logInfo(timedMessage(...args));
+};
 
 console.info('connecting to ' + params.host);
 
