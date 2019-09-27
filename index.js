7cd75fe46ff3038ae000a847ba4af08ff6e6faaa
skazska <skazska@gmail.com>
2019-09-27 11:26:27 +0300

    done forwards

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { Client } = require('ssh2');
 
-const connectionParams = require('./lib/connection-params');
+const {connectionParams, forwardingParams} = require('./lib/connection-params');
 const Controller = require('./lib/controller');
 const argv = require('yargs-parser')(process.argv.slice(2));
 const { timedMessage } = require('./lib/utils');
 
 const conn = new Client();
 const params = connectionParams(argv);
+
+const forwarding = forwardingParams(argv);
 
 const logInfo = console.info;
 console.info = function(...args) {
@@ -20,8 +22,18 @@ conn
         console.info('connected');
         const controller = new Controller({client: conn})
             .on('exit', () => {
+                controller.cleanup();
                 process.exit(0);
             });
+
+        if (forwarding.forward) {
+            controller.forwardDirect(forwarding.forward);
+        }
+
+        if (forwarding.reverse) {
+            controller.forwardReverse(forwarding.reverse);
+        }
+
         controller.shell(process.stdin, process.stdout);
     })
     .connect(params);
